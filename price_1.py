b125ea3e3c647d876e66142258d0d72dd1da5294
WonEoJin <134124807+WonEoJin@users.noreply.github.com>
2023-05-20 17:55:39 +0900

    price_1.py

--- price_1.py
+++ price_1.py
@@ -1,3 +1,16 @@
+from pathlib import Path
+from urllib.request import urlretrieve
+
+데이터가 저장된 텍스트 파일 서버 주소는 다음과 같다.
+
+base_url = "https://raw.githubusercontent.com/codingalzi/pybook/master/jupyter-book/data/"
+
+현재 작업 디렉토리의 `data` 하위 디렉토리에 파일을 다운로드해서 저장할 준비를 한다.
+
+# 저장위치 지정과 생성
+data_path = Path() / "data"
+data_path.mkdir(parents=True, exist_ok=True)
+
 def shopping(shop_file):
     shop_dict = {} # 생성할 사전 객체
 
